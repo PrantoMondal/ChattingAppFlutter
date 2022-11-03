06d3b327e4fa4c7664f123d598792685c4dc0cd6
PrantoMondal <prantomondal517@gmail.com>
2022-11-04 01:16:50 +0600

    profile edit method added

--- lib/pages/user_profile_page.dart
+++ lib/pages/user_profile_page.dart
@@ -7,7 +7,6 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
 
-
 class UserProfilePage extends StatefulWidget {
   static const String routeName = '/userprofile';
 
@@ -18,7 +17,14 @@ class UserProfilePage extends StatefulWidget {
 }
 
 class _UserProfilePageState extends State<UserProfilePage> {
+  final txtController = TextEditingController();
+
   @override
+  void dispose() {
+    txtController.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: const MainDrawer(),
@@ -29,65 +35,80 @@ class _UserProfilePageState extends State<UserProfilePage> {
         child: Consumer<UserProvider>(
           builder: (context, provider, _) =>
               StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
-            stream: provider.getUserById(AuthService.user!.uid),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                final userModel = UserModel.fromMap(snapshot.data!.data()!);
-                return ListView(
-                  children: [
-                    Center(
-                        child: userModel.image == null
-                            ? Image.asset(
-                                'images/person.jpg',
-                                height: 100,
-                                width: 100,
-                                fit: BoxFit.cover,
-                              )
-                            : Image.network(
-                                userModel.image!,
-                                width: 100,
-                                height: 100,
-                                fit: BoxFit.cover,
-                              )),
-                    TextButton.icon(
-                        onPressed: _updateImage,
-                        icon: const Icon(Icons.photo),
-                        label: const Text('Change Image')),
-                    const Divider(
-                      color: Colors.grey,
-                      height: 1,
-                    ),
-                    ListTile(
-                      title: Text(userModel.name ?? 'No Display Name'),
-                      trailing: IconButton(
-                        icon: Icon(Icons.edit),
-                        onPressed: () {},
-                      ),
-                    ),
-                    ListTile(
-                      title: Text(userModel.mobile ?? 'No Mobile Number'),
-                      trailing: IconButton(
-                        icon: Icon(Icons.edit),
-                        onPressed: () {},
-                      ),
-                    ),
-                    ListTile(
-                      title: Text(userModel.email ?? 'No Email Address'),
-                      trailing: IconButton(
-                        icon: Icon(Icons.edit),
-                        onPressed: () {},
-                      ),
-                    )
-                  ],
-                );
-              }
-              if (snapshot.hasError) {
-                return const Text('Failed to fetch data');
-              } else {
-                return const CircularProgressIndicator();
-              }
-            },
-          ),
+                stream: provider.getUserById(AuthService.user!.uid),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    final userModel = UserModel.fromMap(snapshot.data!.data()!);
+                    return ListView(
+                      children: [
+                        Center(
+                            child: userModel.image == null
+                                ? Image.asset(
+                              'images/person.jpg',
+                              height: 100,
+                              width: 100,
+                              fit: BoxFit.cover,
+                            )
+                                : Image.network(
+                              userModel.image!,
+                              width: 100,
+                              height: 100,
+                              fit: BoxFit.cover,
+                            )),
+                        TextButton.icon(
+                            onPressed: _updateImage,
+                            icon: const Icon(Icons.photo),
+                            label: const Text('Change Image')),
+                        const Divider(
+                          color: Colors.grey,
+                          height: 1,
+                        ),
+                        ListTile(
+                          title: Text(userModel.name ?? 'No Display Name'),
+                          trailing: IconButton(
+                            icon: Icon(Icons.edit),
+                            onPressed: () {
+                              showInputDialog('Display Name', userModel.name, (value) {
+                                provider.updateProfile(AuthService.user!.uid,
+                                    {'name': value});
+                              });
+                            },
+                          ),
+                        ),
+                        ListTile(
+                          title: Text(userModel.mobile ?? 'No Mobile Number'),
+                          trailing: IconButton(
+                            icon: Icon(Icons.edit),
+                            onPressed: () {
+                              showInputDialog('Mobile Number', userModel.mobile, (value) {
+                                provider.updateProfile(AuthService.user!.uid,
+                                    {'mobile': value});
+                              });
+                            },
+                          ),
+                        ),
+                        ListTile(
+                          title: Text(userModel.email ?? 'No Email Address'),
+                          trailing: IconButton(
+                            icon: Icon(Icons.edit),
+                            onPressed: () {
+                              showInputDialog('Email', userModel.email, (value) {
+                                provider.updateProfile(AuthService.user!.uid,
+                                    {'email': value});
+                              });
+                            },
+                          ),
+                        )
+                      ],
+                    );
+                  }
+                  if (snapshot.hasError) {
+                    return const Text('Failed to fetch data');
+                  } else {
+                    return const CircularProgressIndicator();
+                  }
+                },
+              ),
         ),
       ),
     );
@@ -99,15 +120,41 @@ class _UserProfilePageState extends State<UserProfilePage> {
     if (xFile != null) {
       try {
         final downloadUrl =
-            await Provider.of<UserProvider>(context, listen: false)
-                .updateImage(xFile);
         await Provider.of<UserProvider>(context, listen: false)
-                .updateProfile(AuthService.user!.uid,
-          {'image' : downloadUrl}
-        );
+            .updateImage(xFile);
+        await Provider.of<UserProvider>(context, listen: false)
+            .updateProfile(AuthService.user!.uid, {'image': downloadUrl});
       } catch (e) {
         rethrow;
       }
     }
+  }
+
+  showInputDialog(String title, String? value, Function(String) onSaved) {
+    txtController.text = value ?? '';
+    showDialog(
+        context: context,
+        builder: (context) =>
+            AlertDialog(
+              title: Text(title),
+              content: Padding(
+                padding: const EdgeInsets.all(8),
+                child: TextField(
+                  controller: txtController,
+                  decoration: InputDecoration(hintText: 'Enter $title'),
+                ),
+              ),
+              actions: [
+                TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: const Text('Cancel')),
+                TextButton(
+                    onPressed: () {
+                      onSaved(txtController.text);
+                      Navigator.pop(context);
+                    },
+                    child: const Text('Update')),
+              ],
+            ));
   }
 }
